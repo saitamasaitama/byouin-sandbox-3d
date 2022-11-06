6338dc1c0779ce211667d76cd24f698e8552f39c
saitamasaitama <s-yoshihara@saitama-tec.co.jp>
2022-11-06 17:32:03 +0900

    h

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-const h= 25;
-const w= 25;
+const h= 36;
+const w= 36;
 const scene=Scene.CreateScene()
 $("Body").append("<div id='Debug'>dev</div>");
 const Debug=$("#Debug");
@@ -110,6 +110,30 @@ function RandomSet(){
 
 }
 
+
+let cX = null;
+let cY = null;
+function InputSet(){
+  $("canvas").on("touchstart",function(e){
+     const x =e.changedTouches[0].pageX
+     const y =e.changedTouches[0].pageY
+     cX=x
+     cY=y
+  });
+  $("canvas").on("touchmove",function(e){
+     const x =e.changedTouches[0].pageX
+     const y =e.changedTouches[0].pageY
+     const diffx = x -cX;
+     const diffy = y -cY;
+     scene.camera.position.x-= diffx*0.05
+     scene.camera.position.z-= diffy*0.05
+     Debug.text(diffx+":"+diffy)
+     cX=x; 
+     cY=y;
+  });
+}
+
+InputSet();
 RandomSet();
 setInterval(function(){scene.Animation()},33)
 setInterval(function(){LifeUpdate() },1000)//alert(88)
